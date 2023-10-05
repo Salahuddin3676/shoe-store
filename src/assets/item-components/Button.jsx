3151c7533da4edb3e5ddc71e4c1235de448e790c
Salahuddin3676 <salahuddin3676@gmail.com>
2023-10-05 19:53:56 +0530

    work in progress

--- src/assets/item-components/Button.jsx
+++ src/assets/item-components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ label, icon }) => {
   return (
-    <button className=" flex items-center gap-3 px-5 py-3 border text-sm rounded-full border-gray-300 duration-300 hover:bg-black hover:text-white font-montserrat uppercase ">
+    <button className=" flex items-center gap-3 px-5 py-3 border text-sm rounded-full border-gray-600 duration-300 hover:bg-black hover:text-white font-montserrat uppercase ">
       {icon} {label}
     </button>
   );
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,28 @@ import { BsArrowRight } from "react-icons/bs";
 
 const Hero = () => {
   return (
-    <section className="w-full h-screen lg:h-auto flex justify-center lg:pt-20 items-center">
-      <div className="flex flex-col items-center justify-center">
-        <h1 className="font-poppins mb-10 lg:-mb-5 text-white font-bold text-5xl lg:text-[150px] drop-shadow-lg ">
-          AirPro X3
-        </h1>
-        <img className=" w-80 md:w-[700px] mb-10" src={shoe1} alt="" />
-        <Button label="shop collection" icon={<BsArrowRight />} />
+    <section className="flex flex-col items-center">
+      <div className="w-full  flex justify-center items-center hero-bg h-screen lg:h-auto lg:py-20">
+        <div className="flex flex-col items-center justify-center gap-10">
+          <div className=" flex flex-col items-center justify-center w-[400px] md:w-[800px]">
+            <h1 className="font-poppins mb-10 lg:-mb-5 text-white font-bold text-5xl lg:text-[150px] drop-shadow-lg ">
+              AirPro X3
+            </h1>
+            <img className=" w-80 md:w-[700px]" src={shoe1} alt="" />
+          </div>
+          <Button label="shop collection" icon={<BsArrowRight />} />
+        </div>
       </div>
+
+      <div className="w-full text-[#3a3a3a] bg-[#fafafa] h-screen py-5 lg:py-24 flex flex-col items-center">
+        <h2 className="font-bold font-poppins text-4xl lg:text-[110px] lg:mb-10 uppercase italic">
+          unbelievable
+        </h2>
+        <h4 className="font-bold font-poppins text-2xl lg:text-5xl uppercase italic">
+          speed. comfort
+        </h4>
+      </div>
+      <div className="w-full h-screen bg-white"></div>
     </section>
   );
 };
