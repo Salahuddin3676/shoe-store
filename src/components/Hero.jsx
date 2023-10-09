33ee00952890a536884b959c72314e87e0cde504
Salahuddin3676 <salahuddin3676@gmail.com>
2023-10-09 22:56:09 +0530

    wip...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import shoe1 from "../assets/images/sports-shoe4.png";
 import Button from "../assets/item-components/Button";
 import { BsArrowRight } from "react-icons/bs";
+import { shoes } from "../data";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { BiSolidStarHalf } from "react-icons/bi";
 
 const Hero = () => {
   return (
     <section className="flex flex-col items-center">
-      <div className="flex items-center justify-center w-full h-screen border border-black hero-bg md:h-auto md:pt-28 md:pb-24 lg:py-40">
+      <div className="flex items-center justify-center w-full h-screen hero-bg md:h-auto md:pt-28 md:pb-24 lg:py-40">
         <div className="flex flex-col items-center justify-center gap-10">
           <div className=" flex flex-col items-center justify-center w-[400px] md:w-[800px]">
             <h1 className="font-poppins mb-10 lg:-mb-5 text-white font-bold text-6xl md:text-8xl lg:text-[150px] drop-shadow-lg ">
@@ -22,13 +25,38 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="w-full text-[#3a3a3a] bg-[#fafafa] h-screen py-5 lg:py-24 flex flex-col items-center">
+      <div className="w-full text-[#3a3a3a] px-6 md:px-16 lg:px-20 bg-[#fafafa] h-auto py-5 lg:py-24 flex flex-col items-center">
         <h2 className="font-bold font-poppins text-4xl lg:text-[110px] lg:mb-10 uppercase italic">
           unbelievable
         </h2>
         <h4 className="text-2xl italic font-bold uppercase font-poppins lg:text-5xl">
           speed. comfort
         </h4>
+
+        <div className="flex flex-wrap items-center w-full h-auto gap-5 p-5 mt-5 md:mt-20 ">
+          {shoes.map((shoe) => {
+            const { id, img, title, rating, shoeType, price } = shoe;
+            return (
+              <div className="w-90% max-w-xs md:max-w-none mx-auto md:w-64 lg:w-[260px] cursor-pointer hover:shadow-lg ">
+                <div>
+                  <img className="rounded-t-md" src={img} alt={title} />
+                </div>
+                <div className="flex rounded-b-md flex-col items-center justify-center gap-1.5 py-10 text-center bg-teal-300 ">
+                  <p className="text-xs font-montserrat">{shoeType}</p>
+                  <h4 className="font-bold font-poppins">{title}</h4>
+                  <div className="flex items-center justify-center gap-0.5 font-bold font-montserrat">
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiFillStar />
+                    <AiOutlineStar />
+                  </div>
+                  <p className="font-semibold font-montserrat">{price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="w-full h-screen bg-white"></div>
     </section>
--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,14 @@
+import shoe1 from "./assets/images/sports-shoe1.jpg";
+import shoe2 from "./assets/images/sports-shoe2.jpg";
+import shoe3 from "./assets/images/sports-shoe3.jpg";
+import shoe4 from "./assets/images/sports-shoe4.jpg";
+import shoe5 from "./assets/images/sports-shoe5.jpg";
+import shoe6 from "./assets/images/sports-shoe6.jpg";
+import shoe7 from "./assets/images/sports-shoe7.jpg";
+import shoe8 from "./assets/images/sports-shoe8.jpg";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { BiSolidStarHalf } from "react-icons/bi";
+
 export const menuLinks = [
   {
     id: 1,
@@ -18,5 +29,72 @@ export const menuLinks = [
     id: 4,
     url: "#contact",
     title: "Contact",
+  },
+];
+
+export const shoes = [
+  {
+    id: 1,
+    img: shoe1,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 4.2,
+    price: "$68.00",
+  },
+  {
+    id: 2,
+    img: shoe2,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 4.4,
+    price: "$68.00",
+  },
+  {
+    id: 3,
+    img: shoe3,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 5,
+    price: "$68.00",
+  },
+  {
+    id: 4,
+    img: shoe4,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 4.1,
+    price: "$68.00",
+  },
+  {
+    id: 5,
+    img: shoe5,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 3.9,
+    price: "$68.00",
+  },
+  {
+    id: 6,
+    img: shoe6,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 4.3,
+    price: "$68.00",
+  },
+  {
+    id: 7,
+    img: shoe7,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 4.4,
+    price: "$68.00",
+  },
+  {
+    id: 8,
+    img: shoe8,
+    shoeType: "Running Shoes",
+    title: "Air Pro X3 Aqua",
+    rating: 5,
+    price: "$68.00",
   },
 ];
